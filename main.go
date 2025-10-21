30eb4981d4490c7229d9c7fbb1d5c8cb9bb2e5c8
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-21 17:44:30 +0700

    feat: next step of minitask

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import "fmt"
 
 func main()  {
-	scores := []int{50, 75, 66, 20, 32, 90}
+	scores := []int{50, 75, 20, 32, 66, 90}
 	editedScores := []int{}
 	editedScores = append(editedScores, scores[:3]...)
 	editedScores = append(editedScores, 88)
 	editedScores = append(editedScores, scores[4:]...)
 
+	// Next Step
+	newScores := []int{}
+	for x := range len(scores) {
+		if (scores[x] == 66) {
+			newScores = append(newScores, scores[:x]...)
+			newScores = append(newScores, 88)
+			newScores = append(newScores, scores[x:]...)
+		}
+	}
+
+	fmt.Println("Result Step 1")
 	for x:= range len(editedScores) {
 		fmt.Println(editedScores[x])
 	}
-	
+
+	fmt.Println("Result Step 2")
+	for x:= range len(newScores) {
+		fmt.Println(newScores[x])
+	}
 }
